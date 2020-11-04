e4edd8394947c8c4d950cc7c972fc1bc31325988
Brian Drake <hacbangtam@gmail.com>
2020-11-04 22:48:36 +0700

    serve css

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(express.static('./public'));
+
 app.get('/', (req, res) => {
-  res.json('Hello');
+  res.sendFile(path.resolve('./public/index.html'));;
 });
 
 app.listen(port);
