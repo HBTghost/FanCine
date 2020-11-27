f3514a65ae97db806f4a88795f26ae5b4236b276
Brian Drake <hacbangtam@gmail.com>
2020-11-27 13:46:23 +0700

    Scss lint (can also fix) traivs (may be)

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 import eslint from 'gulp-eslint';
+import sassLint from 'gulp-sass-lint';
 import nodemon from 'gulp-nodemon';
 import browserSync from 'browser-sync';
 import eslintIfFixed from 'gulp-eslint-if-fixed';
@@ -36,9 +37,9 @@ gulp.task('serve', gulp.series('nodemon', (done) => {
     },
   });
 
-  gulp.watch('public/scss/**/*.scss', gulp.series('compileSCSS'));
+  gulp.watch('public/scss/**/*.scss', gulp.series('compileSCSS', 'lint-sass'));
   gulp.watch('public/njs/**/*.js', gulp.series('compileJS'));
-  gulp.watch(['**/*.js', '!node_modules/**', '!public/js/**'], gulp.series('lint'));
+  gulp.watch(['**/*.js', '!node_modules/**', '!public/js/**'], gulp.series('lint-js-fix'));
   done();
 }));
 
@@ -53,7 +54,7 @@ gulp.task('compileSCSS', () => {
     .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('lint', () => gulp.src(['./**/*.js', '!node_modules/**', '!public/js/**'])
+gulp.task('lint-js-fix', () => gulp.src(['./**/*.js', '!node_modules/**', '!public/js/**'])
   .pipe(eslint(({ useEslintrc: true, fix: true })))
   .pipe(eslint.format())
   .pipe(eslint.failAfterError())
@@ -63,3 +64,13 @@ gulp.task('compileJS', () => gulp.src('public/njs/**/*.js')
   .pipe(babel())
   .pipe(minify())
   .pipe(gulp.dest('public/js')));
+
+gulp.task('lint-js', () => gulp.src(['./**/*.js', '!node_modules/**', '!public/js/**'])
+  .pipe(eslint(({ useEslintrc: true })))
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError()));
+
+gulp.task('lint-sass', () => gulp.src('public/scss/**/*.scss')
+  .pipe(sassLint({ configFile: '.sass-lint.yml' }))
+  .pipe(sassLint.format())
+  .pipe(sassLint.failOnError()));
