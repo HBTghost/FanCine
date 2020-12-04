7f13ae1c9148c3534dd1a14aad33440c4d7344df
Brian Drake <hacbangtam@gmail.com>
2020-12-05 00:52:33 +0700

    Change admin email address

--- server/_helpers/db.js
+++ server/_helpers/db.js
@@ -1,26 +1,6 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import config from '../config.json';
-
 import Account from '../accounts/account.model.js';
 import RefreshToken from '../accounts/refresh-token.model.js';
-
-dotenv.config();
-
-const connectionOptions = {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-};
-
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
-
-const db = mongoose.connection;
-db.on('error', (err) => console.error(err));
-db.once('open', () => console.log('Connected to Database'));
-
-mongoose.Promise = global.Promise;
 
 function isValidId(id) {
   return mongoose.Types.ObjectId.isValid(id);
