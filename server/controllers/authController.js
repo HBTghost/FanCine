2cb519a8771624498622b8d6ac516aa37a21aa9f
Brian Drake <hacbangtam@gmail.com>
2020-12-06 16:13:10 +0700

    Oauth2 Google API

---
+++ server/controllers/authController.js
@@ -0,0 +1,382 @@
+import passport from 'passport';
+import bcryptjs from 'bcryptjs';
+import nodemailer from 'nodemailer';
+import { google } from 'googleapis';
+import jwt from 'jsonwebtoken';
+
+// ------------ User Model ------------//
+import User from '../models/User.js';
+
+const { OAuth2 } = google.auth;
+const JWT_KEY = 'jwtactive987';
+const JWT_RESET_KEY = 'jwtreset987';
+
+// ------------ Register Handle ------------//
+function registerHandle(req, res) {
+  const {
+    name, email, password, password2,
+  } = req.body;
+  const errors = [];
+
+  // ------------ Checking required fields ------------//
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please enter all fields' });
+  }
+
+  // ------------ Checking password mismatch ------------//
+  if (password !== password2) {
+    errors.push({ msg: 'Passwords do not match' });
+  }
+
+  // ------------ Checking password length ------------//
+  if (password.length < 8) {
+    errors.push({ msg: 'Password must be at least 8 characters' });
+  }
+
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2,
+    });
+  } else {
+    // ------------ Validation passed ------------//
+    User.findOne({ email }).then((user) => {
+      if (user) {
+        // ------------ User already exists ------------//
+        errors.push({ msg: 'Email ID already registered' });
+        res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          password2,
+        });
+      } else {
+        const oauth2Client = new OAuth2(
+          '173872994719-pvsnau5mbj47h0c6ea6ojrl7gjqq1908.apps.googleusercontent.com', // ClientID
+          'OKXIYR14wBB_zumf30EC__iJ', // Client Secret
+          'https://developers.google.com/oauthplayground', // Redirect URL
+        );
+
+        oauth2Client.setCredentials({
+          refresh_token: '1//04T_nqlj9UVrVCgYIARAAGAQSNwF-L9IrGm-NOdEKBOakzMn1cbbCHgg2ivkad3Q_hMyBkSQen0b5ABfR8kPR18aOoqhRrSlPm9w',
+        });
+        const accessToken = oauth2Client.getAccessToken();
+
+        const token = jwt.sign({ name, email, password }, JWT_KEY, { expiresIn: '30m' });
+        const CLIENT_URL = `http://${req.headers.host}`;
+
+        const output = `
+                <h2>Please click on below link to activate your account</h2>
+                <p>${CLIENT_URL}/activate/${token}</p>
+                <p><b>NOTE: </b> The above activation link expires in 30 minutes.</p>
+                `;
+
+        const transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            type: 'OAuth2',
+            user: 'nodejsa@gmail.com',
+            clientId: '173872994719-pvsnau5mbj47h0c6ea6ojrl7gjqq1908.apps.googleusercontent.com',
+            clientSecret: 'OKXIYR14wBB_zumf30EC__iJ',
+            refreshToken: '1//04T_nqlj9UVrVCgYIARAAGAQSNwF-L9IrGm-NOdEKBOakzMn1cbbCHgg2ivkad3Q_hMyBkSQen0b5ABfR8kPR18aOoqhRrSlPm9w',
+            accessToken,
+          },
+        });
+
+        // send mail with defined transport object
+        const mailOptions = {
+          from: '"Auth Admin" <fancine@gmail.com>', // sender address
+          to: email, // list of receivers
+          subject: 'Account Verification: NodeJS Auth ✔', // Subject line
+          generateTextFromHTML: true,
+          html: output, // html body
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.log(error);
+            req.flash(
+              'error_msg',
+              'Something went wrong on our end. Please register again.',
+            );
+            res.redirect('/login');
+          } else {
+            console.log('Mail sent : %s', info.response);
+            req.flash(
+              'success_msg',
+              'Activation link sent to email ID. Please activate to log in.',
+            );
+            res.redirect('/login');
+          }
+        });
+      }
+    });
+  }
+}
+
+// ------------ Activate Account Handle ------------//
+function activateHandle(req, res) {
+  const { token } = req.params;
+  const errors = [];
+  if (token) {
+    jwt.verify(token, JWT_KEY, (err, decodedToken) => {
+      if (err) {
+        req.flash(
+          'error_msg',
+          'Incorrect or expired link! Please register again.',
+        );
+        res.redirect('/register');
+      } else {
+        const { name, email, password } = decodedToken;
+        User.findOne({ email }).then((user) => {
+          if (user) {
+            // ------------ User already exists ------------//
+            req.flash(
+              'error_msg',
+              'Email ID already registered! Please log in.',
+            );
+            res.redirect('/login');
+          } else {
+            const newUser = new User({
+              name,
+              email,
+              password,
+            });
+
+            bcryptjs.genSalt(10, (err1, salt) => {
+              bcryptjs.hash(newUser.password, salt, (err2, hash) => {
+                if (err2) throw err2;
+                newUser.password = hash;
+                newUser
+                  .save()
+                  .then((user1) => {
+                    req.flash(
+                      'success_msg',
+                      'Account activated. You can now log in.',
+                    );
+                    res.redirect('/login');
+                  })
+                  .catch((err3) => console.log(err3));
+              });
+            });
+          }
+        });
+      }
+    });
+  } else {
+    console.log('Account activation error!');
+  }
+}
+
+// ------------ Forgot Password Handle ------------//
+function forgotPassword(req, res) {
+  const { email } = req.body;
+
+  const errors = [];
+
+  // ------------ Checking required fields ------------//
+  if (!email) {
+    errors.push({ msg: 'Please enter an email ID' });
+  }
+
+  if (errors.length > 0) {
+    res.render('forgot', {
+      errors,
+      email,
+    });
+  } else {
+    User.findOne({ email }).then((user) => {
+      if (!user) {
+        // ------------ User already exists ------------//
+        errors.push({ msg: 'User with Email ID does not exist!' });
+        res.render('forgot', {
+          errors,
+          email,
+        });
+      } else {
+        const oauth2Client = new OAuth2(
+          '173872994719-pvsnau5mbj47h0c6ea6ojrl7gjqq1908.apps.googleusercontent.com', // ClientID
+          'OKXIYR14wBB_zumf30EC__iJ', // Client Secret
+          'https://developers.google.com/oauthplayground', // Redirect URL
+        );
+
+        oauth2Client.setCredentials({
+          refresh_token: '1//04T_nqlj9UVrVCgYIARAAGAQSNwF-L9IrGm-NOdEKBOakzMn1cbbCHgg2ivkad3Q_hMyBkSQen0b5ABfR8kPR18aOoqhRrSlPm9w',
+        });
+        const accessToken = oauth2Client.getAccessToken();
+
+        const token = jwt.sign({ _id: user._id }, JWT_RESET_KEY, { expiresIn: '30m' });
+        const CLIENT_URL = `http://${req.headers.host}`;
+        const output = `
+                <h2>Please click on below link to reset your account password</h2>
+                <p>${CLIENT_URL}/forgot/${token}</p>
+                <p><b>NOTE: </b> The activation link expires in 30 minutes.</p>
+                `;
+
+        User.updateOne({ resetLink: token }, (err, success) => {
+          if (err) {
+            errors.push({ msg: 'Error resetting password!' });
+            res.render('forgot', {
+              errors,
+              email,
+            });
+          } else {
+            const transporter = nodemailer.createTransport({
+              service: 'gmail',
+              auth: {
+                type: 'OAuth2',
+                user: 'nodejsa@gmail.com',
+                clientId: '173872994719-pvsnau5mbj47h0c6ea6ojrl7gjqq1908.apps.googleusercontent.com',
+                clientSecret: 'OKXIYR14wBB_zumf30EC__iJ',
+                refreshToken: '1//04T_nqlj9UVrVCgYIARAAGAQSNwF-L9IrGm-NOdEKBOakzMn1cbbCHgg2ivkad3Q_hMyBkSQen0b5ABfR8kPR18aOoqhRrSlPm9w',
+                accessToken,
+              },
+            });
+
+            // send mail with defined transport object
+            const mailOptions = {
+              from: '"Auth Admin" <fancine@gmail.com>', // sender address
+              to: email, // list of receivers
+              subject: 'Account Password Reset: NodeJS Auth ✔', // Subject line
+              html: output, // html body
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+              if (error) {
+                console.log(error);
+                req.flash(
+                  'error_msg',
+                  'Something went wrong on our end. Please try again later.',
+                );
+                res.redirect('/forgot');
+              } else {
+                console.log('Mail sent : %s', info.response);
+                req.flash(
+                  'success_msg',
+                  'Password reset link sent to email ID. Please follow the instructions.',
+                );
+                res.redirect('/login');
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+}
+
+// ------------ Redirect to Reset Handle ------------//
+function gotoReset(req, res) {
+  const { token } = req.params;
+
+  if (token) {
+    jwt.verify(token, JWT_RESET_KEY, (err, decodedToken) => {
+      if (err) {
+        req.flash(
+          'error_msg',
+          'Incorrect or expired link! Please try again.',
+        );
+        res.redirect('/login');
+      } else {
+        const { _id } = decodedToken;
+        User.findById(_id, (err1, user) => {
+          if (err1) {
+            req.flash(
+              'error_msg',
+              'User with email ID does not exist! Please try again.',
+            );
+            res.redirect('/login');
+          } else {
+            res.redirect(`/reset/${_id}`);
+          }
+        });
+      }
+    });
+  } else {
+    console.log('Password reset error!');
+  }
+}
+
+function resetPassword(req, res) {
+  let { password, password2 } = req.body;
+  const { id } = req.params;
+  const errors = [];
+
+  // ------------ Checking required fields ------------//
+  if (!password || !password2) {
+    req.flash(
+      'error_msg',
+      'Please enter all fields.',
+    );
+    res.redirect(`/reset/${id}`);
+  } else if (password.length < 8) {
+    req.flash(
+      'error_msg',
+      'Password must be at least 8 characters.',
+    );
+    res.redirect(`/reset/${id}`);
+  } else if (password !== password2) {
+    req.flash(
+      'error_msg',
+      'Passwords do not match.',
+    );
+    res.redirect(`/reset/${id}`);
+  } else {
+    bcryptjs.genSalt(10, (err, salt) => {
+      bcryptjs.hash(password, salt, (err1, hash) => {
+        if (err1) throw err1;
+        password = hash;
+
+        User.findByIdAndUpdate(
+          { _id: id },
+          { password },
+          (err2, result) => {
+            if (err2) {
+              req.flash(
+                'error_msg',
+                'Error resetting password!',
+              );
+              res.redirect(`/reset/${id}`);
+            } else {
+              req.flash(
+                'success_msg',
+                'Password reset successfully!',
+              );
+              res.redirect('/login');
+            }
+          },
+        );
+      });
+    });
+  }
+}
+
+// ------------ Login Handle ------------//
+function loginHandle(req, res, next) {
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true,
+  })(req, res, next);
+}
+
+// ------------ Logout Handle ------------//
+function logoutHandle(req, res) {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/login');
+}
+
+export default {
+  registerHandle,
+  activateHandle,
+  forgotPassword,
+  gotoReset,
+  resetPassword,
+  loginHandle,
+  logoutHandle,
+};
--- server/routes/handler.js
+++ server/routes/handler.js
@@ -1,5 +1,6 @@
 import express from 'express';
 
+import { ensureAuthenticated, forwardAuthenticated } from '../config/checkAuth.js';
 import { getMovie, getAllMovies, getMoviesByTheaterID } from '../middleware/movie.js';
 import { getAllTheaters, getTheatersByMovieID } from '../middleware/theater.js';
 import {
@@ -16,6 +17,7 @@ handlebarsRouter.get('/', getAllMovies, async (req, res) => {
   res.render('home', {
     style: 'home',
     movies: await res.allMovies,
+    username: req.user ? req.user.name : undefined,
   });
 });
 
@@ -23,6 +25,7 @@ handlebarsRouter.get('/info/:id', getMovie, (req, res) => {
   res.render('info', {
     style: 'info',
     movie: res.movie,
+    username: req.user ? req.user.name : undefined,
   });
 });
 
@@ -30,34 +33,39 @@ handlebarsRouter.get('/movie-on-show', getAllMovies, async (req, res) => {
   res.render('movieLiveList', {
     style: 'movie-on-show',
     movies: await res.allMovies,
+    username: req.user ? req.user.name : undefined,
   });
 });
 
 handlebarsRouter.get('/review', (req, res) => {
   res.render('review', {
     style: 'review',
+    username: req.user ? req.user.name : undefined,
   });
 });
 
 handlebarsRouter.get('/review/reviewContentSample', (req, res) => {
   res.render('partials/reviewContentSample', {
     style: 'reviewContent',
+    username: req.user ? req.user.name : undefined,
   });
 });
 
 handlebarsRouter.get('/info', (req, res) => {
   res.render('info', {
     style: 'info',
+    username: req.user ? req.user.name : undefined,
   });
 });
 
-handlebarsRouter.get('/book-ticket', (req, res) => {
+handlebarsRouter.get('/book-ticket', ensureAuthenticated, (req, res) => {
   res.render('book-ticket', {
     style: 'book-ticket',
+    username: req.user ? req.user.name : undefined,
   });
 });
 handlebarsRouter.get(
-  '/book-ticket/:_idTheaterMovie/:_idDateShow/:_idTypeShow/:time/',
+  '/book-ticket/:_idTheaterMovie/:_idDateShow/:_idTypeShow/:time/', ensureAuthenticated,
   getShowTimeByOtherKey,
   (req, res) => {
     // res.render('book-ticket', {
@@ -69,12 +77,14 @@ handlebarsRouter.get(
 handlebarsRouter.get('/book-ticket', (req, res) => {
   res.render('book-ticket', {
     style: 'book-ticket',
+    username: req.user ? req.user.name : undefined,
   });
 });
 
 handlebarsRouter.get('/book-ticket/:id', (req, res) => {
   res.render('book-ticket', {
     style: 'book-ticket',
+    username: req.user ? req.user.name : undefined,
   });
 });
 
@@ -82,6 +92,7 @@ handlebarsRouter.get('/theaters', getAllTheaters, (req, res) => {
   res.render('theaters', {
     style: 'theaters',
     theaters: res.allTheaters,
+    username: req.user ? req.user.name : undefined,
   });
 });
 
@@ -89,6 +100,7 @@ handlebarsRouter.get('/data', getAllMovies, (req, res) => {
   try {
     res.render('data', {
       movies: res.allMovies,
+      username: req.user ? req.user.name : undefined,
     });
   } catch (err) {
     console.log(err);
@@ -100,6 +112,7 @@ handlebarsRouter.get('/showtimes', async (req, res) => {
   res.render('showtimes', {
     style: 'showtimes',
     script: 'showtimes',
+    username: req.user ? req.user.name : undefined,
   });
 });
 
@@ -108,6 +121,7 @@ handlebarsRouter.get('/showtimes/allMovies', getAllMovies, (req, res) => {
   res.header('Content-Type', 'text/html');
   res.render('partials/renderStructure/showtimes/movies', {
     movies: res.allMovies,
+    username: req.user ? req.user.name : undefined,
   });
 });
 
@@ -116,6 +130,7 @@ handlebarsRouter.get('/showtimes/allMovies/:id', getTheaterMoviesByTheaterID, ge
   res.header('Content-Type', 'text/html');
   res.render('partials/renderStructure/showtimes/movies', {
     movies: res.movies,
+    username: req.user ? req.user.name : undefined,
   });
 });
 
@@ -124,6 +139,7 @@ handlebarsRouter.get('/showtimes/allTheaters', getAllTheaters, (req, res) => {
   res.header('Content-Type', 'text/html');
   res.render('partials/renderStructure/showtimes/theaters', {
     theaters: res.allTheaters,
+    username: req.user ? req.user.name : undefined,
   });
 });
 
@@ -132,6 +148,7 @@ handlebarsRouter.get('/showtimes/allTheaters/:id', getTheaterMoviesByMovieID, ge
   res.header('Content-Type', 'text/html');
   res.render('partials/renderStructure/showtimes/theaters', {
     theaters: res.theaters,
+    username: req.user ? req.user.name : undefined,
   });
 });
 
@@ -151,11 +168,16 @@ handlebarsRouter.get('/showtimes/allShowTimes/:id', getTheaterMovieRecursively, 
   res.header('Content-Type', 'text/html');
   res.render('partials/renderStructure/showtimes/showtimes', {
     theaterMovieRecursively: res.theaterMovie,
+    username: req.user ? req.user.name : undefined,
   });
 });
 
+handlebarsRouter.get('/authenticate', forwardAuthenticated, (req, res) => {
+  res.render('authenticate');
+});
+
 // Member
-handlebarsRouter.all('/member', (req, res) => {
+handlebarsRouter.all('/member', ensureAuthenticated, (req, res) => {
   res.render('member', {
     style: 'member',
     userInfo: {
@@ -168,6 +190,7 @@ handlebarsRouter.all('/member', (req, res) => {
       email: 'dinhtranvana2000@gmail.com',
       curYear: '2020',
     },
+    username: req.user ? req.user.name : undefined,
   });
 });
 
