3bc8193a85ca752bda7faa44a3ada667c470cf95
Nguyễn Đức Hiếu <51395700+18127099@users.noreply.github.com>
2021-01-12 06:43:47 +0700

    feat(manage movie): design modal and basic functions

--- public/js/pages/manageMovie.js
+++ public/js/pages/manageMovie.js
@@ -24,6 +24,28 @@ function refreshDisplay() {
       hideTableColumn(i + 1);
     }
   });
+}
+
+function openModal() {
+  $('#myModal').css('display', 'block');
+}
+
+function closeModal() {
+  $('#myModal').css('display', 'none');
+}
+
+function editModal() {
+  if (!$('.modal-content').hasClass('editing')) {
+    $('.modal-content').addClass('editing');
+    $('#button-submit').css('display', 'none');
+    $('#close').css('display', 'none');
+    $('#edit').text('Save');
+  } else {
+    $('.modal-content').removeClass('editing');
+    $('#edit').text('Edit');
+    $('#button-submit').css('display', 'inline-block');
+    $('#close').css('display', 'inline-block');
+  }
 } // ===== Events =====
 
 
@@ -39,8 +61,55 @@ function eventDisplayCheckboxes() {
   });
 }
 
+function eventRowMovies() {
+  movieRowElements.forEach(function (row) {
+    row.addEventListener('click', function () {
+      var _id = row.children[13].innerHTML;
+      fetch("/api/movies/".concat(_id)).then(function (res) {
+        res.json().then(function (data) {
+          $('h2').html(data.originalName);
+          $('.idField').html(_id);
+          $('.originalNameField').html(data.originalName);
+          $('.vietnameseNameField').html(data.vietnameseName);
+          $('.ratingField').html(data.rating);
+          $('.ratesField').html(data.rates);
+          $('.labelField').html(data.label);
+          $('.timeField').html(data.time);
+          $('.producerField').html(data.producer);
+          $('.categoryField').html(data.category[0]);
+
+          for (var i = 1; i < data.category.length; i += 1) {
+            $('.categoryField').append(", ".concat(data.category[i]));
+          }
+
+          $('.castField').html(data.cast[0]);
+
+          for (var _i = 1; _i < data.cast.length; _i += 1) {
+            $('.castField').append(", ".concat(data.cast[_i]));
+          }
+
+          $('.nationField').html(data.nation);
+          $('.directorField').html(data.director);
+          $('.dateField').html(data.date);
+          $('.descriptionField').html(data.description[0]);
+
+          for (var _i2 = 1; _i2 < data.description.length; _i2 += 1) {
+            $('.descriptionField').append("\n".concat(data.description[_i2]));
+          } // $('.trailerEmbedIDField').html(data.trailerEmbedID);
+          // $('.imageSourceField').html(data.imageSource);
+          // $('.horizontalImageSourceField').html(data.horizontalImageSource);
+
+
+          openModal();
+        });
+      });
+    });
+  });
+}
+
 function handleEvents() {
   eventDisplayCheckboxes();
+  eventRowMovies();
 } // ===== Main =====
 
 
--- public/njs/pages/manageMovie.js
+++ public/njs/pages/manageMovie.js
@@ -27,6 +27,28 @@ function refreshDisplay() {
   });
 }
 
+function openModal() {
+  $('#myModal').css('display', 'block');
+}
+
+function closeModal() {
+  $('#myModal').css('display', 'none');
+}
+
+function editModal() {
+  if (!($('.modal-content').hasClass('editing'))) {
+    $('.modal-content').addClass('editing');
+    $('#button-submit').css('display', 'none');
+    $('#close').css('display', 'none');
+    $('#edit').text('Save');
+  } else {
+    $('.modal-content').removeClass('editing');
+    $('#edit').text('Edit');
+    $('#button-submit').css('display', 'inline-block');
+    $('#close').css('display', 'inline-block');
+  }
+}
+
 // ===== Events =====
 function eventDisplayCheckboxes() {
   displayCheckboxElements.forEach((e, i) => {
@@ -40,8 +62,49 @@ function eventDisplayCheckboxes() {
   });
 }
 
+function eventRowMovies() {
+  movieRowElements.forEach((row) => {
+    row.addEventListener('click', () => {
+      const _id = row.children[13].innerHTML;
+      fetch(`/api/movies/${_id}`).then((res) => {
+        res.json().then((data) => {
+          $('h2').html(data.originalName);
+          $('.idField').html(_id);
+          $('.originalNameField').html(data.originalName);
+          $('.vietnameseNameField').html(data.vietnameseName);
+          $('.ratingField').html(data.rating);
+          $('.ratesField').html(data.rates);
+          $('.labelField').html(data.label);
+          $('.timeField').html(data.time);
+          $('.producerField').html(data.producer);
+          $('.categoryField').html(data.category[0]);
+          for (let i = 1; i < data.category.length; i += 1) {
+            $('.categoryField').append(`, ${data.category[i]}`);
+          }
+          $('.castField').html(data.cast[0]);
+          for (let i = 1; i < data.cast.length; i += 1) {
+            $('.castField').append(`, ${data.cast[i]}`);
+          }
+          $('.nationField').html(data.nation);
+          $('.directorField').html(data.director);
+          $('.dateField').html(data.date);
+          $('.descriptionField').html(data.description[0]);
+          for (let i = 1; i < data.description.length; i += 1) {
+            $('.descriptionField').append(`\n${data.description[i]}`);
+          }
+          // $('.trailerEmbedIDField').html(data.trailerEmbedID);
+          // $('.imageSourceField').html(data.imageSource);
+          // $('.horizontalImageSourceField').html(data.horizontalImageSource);
+          openModal();
+        });
+      });
+    });
+  });
+}
+
 function handleEvents() {
   eventDisplayCheckboxes();
+  eventRowMovies();
 }
 
 // ===== Main =====
